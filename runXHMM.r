d79543d35af31360cf0410bfc3556169614f6690
johnbwaldron <39961096+johnbwaldron@users.noreply.github.com>
2018-07-15 14:54:55 -0500

    Update runXHMM.r

--- runXHMM.r
+++ runXHMM.r
@@ -1,3 +1,26 @@
+#https://software.broadinstitute.org/gatk/documentation/tooldocs/current/
+# XHMM workflow and install
+# http://atgu.mgh.harvard.edu/xhmm/download.shtml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Rterm.exe --vanilla
 # https://cran.r-project.org/web/packages/xhmmScripts/xhmmScripts.pdf
 # install.packages("xhmmScripts")
