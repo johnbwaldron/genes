c8698e2d99d92db3873d36e742172ed12d676e5f
johnbwaldron <39961096+johnbwaldron@users.noreply.github.com>
2018-07-15 15:23:42 -0500

    Update runXHMM.r

--- runXHMM.r
+++ runXHMM.r
@@ -3,10 +3,20 @@
 # http://atgu.mgh.harvard.edu/xhmm/download.shtml
 
 
-
-
-
-
+wget https://bitbucket.org/statgen/xhmm/get/cc14e528d909.zip
+unzip cc14e528d909.zip
+cd statgen-xhmm-cc14e528d909
+make
+7/15/18
+######################################################################################################################################
+/usr/bin/ld: cannot find -llapack
+collect2: error: ld returned 1 exit status
+Makefile:142: recipe for target 'build/execs/xhmm' failed
+make[1]: *** [build/execs/xhmm] Error 1
+make[1]: Leaving directory '/home/jwaldr/statgen-xhmm-cc14e528d909'
+Makefile:121: recipe for target 'all' failed
+make: *** [all] Error 2
+#######################################################################################################################################
 
 
 
